c59ba320b677295689f5b8e9046683ea5203f793
veereshchakali <veeresh4600@gmail.com>
2023-07-28 12:07:12 +0530

    eager loading

---
+++ src/app/about/about-ceo/about-ceo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-ceo',
+  templateUrl: './about-ceo.component.html',
+  styleUrls: ['./about-ceo.component.scss']
+})
+export class AboutCeoComponent {
+
+}
